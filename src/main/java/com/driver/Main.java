e9291c34d0efa861f2a5c3e6d250b64c934a936e
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-03-02 17:14:02 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -23,19 +23,3 @@ public class Main {
         }
     }
 }
-
-
-
-package com.driver;
-
-public class RWOnly {
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
